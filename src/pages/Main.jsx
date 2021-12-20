70696ba11bb2dd6755e560b37824f37295d02db7
Arthur Viegas <viegas.arthur2013@hotmail.com>
2021-12-20 19:04:07 -0300

    Define main section positions

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -7,6 +7,20 @@ function Main() {
   return (
     <>
     <Header />
+    <main>
+    <section className="about-section">
+      <div className="my-description">
+        <div>
+          <h3 id="teste">Hi! I am </h3>
+          <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui 
+            officia deserunt mollit anim id est laborum."</p>
+        </div>
+      </div>
+      <div className="my-img">
+        <img src={profile} alt="minha foto" />
+      </div>
+    </section>
+    </main>
     </>
   );
 }
