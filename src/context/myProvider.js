51bff0e631b57099d32a08fd361a5aa4a95ba832
Arthur Viegas <viegas.arthur2013@hotmail.com>
2021-12-20 19:46:20 -0300

    teste

--- src/context/myProvider.js
+++ src/context/myProvider.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import MyContext from "./MyContext";
-
+const teste = 'oi'
 function MyProvider({ children }) {
   return (
-    <MyContext.Provider value={{}} >
+    <MyContext.Provider value={{ teste }} >
       { children }
     </MyContext.Provider>
   )
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Header from '../components/Header';
 import profile from '../assets/images/profile.jpeg'
 import '../assets/css/mainPage.css';
+import MyContext from '../context/MyContext';
 
 function Main() {
+  const { teste } = useContext(MyContext)
   return (
     <>
     <Header />
@@ -11,6 +13,7 @@ function Main() {
     <section className="about-section">
       <div className="my-description">
         <div>
+          {console.log(teste)}
           <h3 id="teste">Hi! I am </h3>
           <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui 
             officia deserunt mollit anim id est laborum."</p>
